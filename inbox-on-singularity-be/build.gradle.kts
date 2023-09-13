7fc7047ea94a9fa0a2fbf62467d8ad352dbaac2b
Dmytro Tykhonov <dmytro@fishfacts.fo>
2023-09-13 13:23:23 +0200

    fixed the checkstyle

--- inbox-on-singularity-be/build.gradle.kts
+++ inbox-on-singularity-be/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     java
-    id("com.palantir.docker") version "0.28.0"
+    id("com.palantir.docker") version "0.35.0"
 }
 
 repositories {
@@ -20,12 +20,14 @@ dependencies {
     testImplementation("org.slf4j:slf4j-log4j12:1.7.30")
 }
 
+/*
 application {
-    mainClassName = "com.ibtsoft.inbox.main.InboxSocketIoApplication"
+    mainClass = com.ibtsoft.inbox.main.InboxSocketIoApplication
 }
+*/
 
-java.sourceCompatibility = JavaVersion.VERSION_11
-java.targetCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 tasks.jar {
     manifest {
